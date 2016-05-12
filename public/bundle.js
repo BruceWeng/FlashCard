7945d2b8dc5b76e9df09ef956025a7e233b5235b
BruceWeng <weng8916@gmail.com>
2016-05-12 12:22:45 -0700

    Combine Reducers and Actions

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,45 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-console.log("Hello React");
+var cards = function cards(state, action) {
+  switch (action.type) {
+    case 'ADD_CARD':
+      var newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date()
+      });
+
+      return state.concat([newCard]);
+    default:
+      return state || [];
+  }
+};
+
+var store = Redux.createStore(Redux.combineReducers({
+  cards: cards
+}));
+/*
+function (state, action) {
+  cards: cards(state.cards, aciton),
+  decks: decks(state.decks, action)
+};
+*/
+
+store.subscribe(function () {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {
+    front: 'front',
+    back: 'back'
+  }
+});
+
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {}
+});
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
-console.log("Hello React");
+const cards = (state, action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date
+      });
+
+      return state.concat([newCard]);
+    default:
+      return state || [];
+  }
+}
+
+const store = Redux.createStore(Redux.combineReducers({
+  cards: cards
+}));
+/*
+function (state, action) {
+  cards: cards(state.cards, aciton),
+  decks: decks(state.decks, action)
+};
+*/
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {
+    front: 'front',
+    back: 'back'
+  }
+});
+
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {
+  }
+});
