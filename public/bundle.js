c40c38db64d1f77c53e2c7d4d80819210cf9fd8c
BruceWeng <weng8916@gmail.com>
2016-05-12 15:42:08 -0700

    Write and use action creators

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,16 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var _addDeck = function _addDeck(name) {
+  return { type: 'ADD_DECK', data: name };
+};
+var _showAddDeck = function _showAddDeck() {
+  return { type: 'SHOW_ADD_DECK' };
+};
+var _hideAddDeck = function _hideAddDeck() {
+  return { type: 'HIDE_ADD_DECK' };
+};
+
 var cards = function cards(state, action) {
   switch (action.type) {
     case 'ADD_CARD':
@@ -15,8 +25,31 @@ var cards = function cards(state, action) {
   }
 };
 
+var decks = function decks(state, action) {
+  switch (action.type) {
+    case 'ADD_DECK':
+      var newDeck = { name: action.data, id: +new Date() };
+      return state.concat([newDeck]);
+    default:
+      return state || [];
+  }
+};
+
+var addingDeck = function addingDeck(state, action) {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK':
+      return true;
+    case 'HIDE_ADD_DECK':
+      return false;
+    default:
+      return !!state;
+  }
+};
+
 var store = Redux.createStore(Redux.combineReducers({
-  cards: cards
+  cards: cards,
+  decks: decks,
+  addingDeck: addingDeck
 }));
 /*
 function (state, action) {
@@ -32,10 +65,17 @@ var App = function App(props) {
     props.children
   );
 };
-
+/*THe reason we use React.createClass here rather than pure function
+is that we will use life cycle methods*/
 var Sidebar = React.createClass({
   displayName: 'Sidebar',
+  componentDidUpdate: function componentDidUpdate() {
+    var el = ReactDOM.findDOMNode(this.refs.add);
+    if (el) el.focus();
+  },
   render: function render() {
+    var _this = this;
+
     var props = this.props;
 
     return React.createElement(
@@ -45,6 +85,13 @@ var Sidebar = React.createClass({
         'h2',
         null,
         ' All Decks '
+      ),
+      React.createElement(
+        'button',
+        { onClick: function onClick(e) {
+            return _this.props.showAddDeck();
+          } },
+        'New Deck'
       ),
       React.createElement(
         'ul',
@@ -59,16 +106,39 @@ var Sidebar = React.createClass({
           );
         })
       ),
-      props.addingDeck && React.createElement('input', { ref: 'add' })
+      props.addingDeck && React.createElement('input', { ref: 'add', onKeyPress: this.createDeck })
     );
+  },
+  createDeck: function createDeck(evt) {
+    if (evt.which != 13) return;
+
+    var name = ReactDOM.findDOMNode(this.refs.add).value;
+    this.props.addDeck(name);
+    this.props.hideAddDeck();
   }
 });
+function run() {
+  var state = store.getState();
+  ReactDOM.render(React.createElement(
+    App,
+    null,
+    React.createElement(Sidebar, {
+      decks: state.decks,
+      addingDeck: state.addingDeck,
+      addDeck: function addDeck(name) {
+        return store.dispatch(_addDeck(name));
+      },
+      showAddDeck: function showAddDeck() {
+        return store.dispatch(_showAddDeck());
+      },
+      hideAddDeck: function hideAddDeck() {
+        return store.dispatch(_hideAddDeck());
+      }
+    })
+  ), document.getElementById('root'));
+}
 
-ReactDOM.render(React.createElement(
-  App,
-  null,
-  ' ',
-  React.createElement(Sidebar, { decks: [{ name: 'Deck 1' }], addingDeck: true })
-), document.getElementById('root'));
+run();
+store.subscribe(run);
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+const addDeck = name => ({ type: 'ADD_DECK', data: name});
+const showAddDeck = () => ({ type: 'SHOW_ADD_DECK'});
+const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK'});
+
 const cards = (state, action) => {
   switch (action.type) {
     case 'ADD_CARD':
@@ -10,10 +14,29 @@ const cards = (state, action) => {
     default:
       return state || [];
   }
-}
+};
+
+const decks = (state, action) => {
+  switch (action.type) {
+    case 'ADD_DECK':
+    let newDeck = { name: action.data, id: +new Date };
+    return state.concat([newDeck]);
+    default: return state || [];
+  }
+};
+
+const addingDeck = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK': return true;
+    case 'HIDE_ADD_DECK': return false;
+    default: return !!state;
+  }
+};
 
 const store = Redux.createStore(Redux.combineReducers({
-  cards: cards
+  cards,
+  decks,
+  addingDeck
 }));
 /*
 function (state, action) {
@@ -29,24 +52,52 @@ const App = (props) => {
     </div>
   )
 };
-
+/*THe reason we use React.createClass here rather than pure function
+is that we will use life cycle methods*/
 const Sidebar = React.createClass({
+  componentDidUpdate() {
+    var el = ReactDOM.findDOMNode(this.refs.add);
+    if (el) el.focus();
+  },
+
   render() {
     let props = this.props;
 
     return(
       <div className='sidebar'>
         <h2> All Decks </h2>
+        <button onClick={ e => this.props.showAddDeck()}>
+          New Deck
+        </button>
         <ul>
           {props.decks.map((deck, i) =>
             <li key={i}> {deck.name} </li>
           )}
         </ul>
-        {props.addingDeck && <input ref='add' />}
+        {props.addingDeck && <input ref='add' onKeyPress={this.createDeck} />}
       </div>
     );
+  },
+  createDeck(evt) {
+    if (evt.which != 13) return;
+
+    var name = ReactDOM.findDOMNode(this.refs.add).value;
+    this.props.addDeck(name);
+    this.props.hideAddDeck();
   }
 });
+function run () {
+  let state = store.getState();
+  ReactDOM.render(<App>
+    <Sidebar
+      decks={state.decks}
+      addingDeck={state.addingDeck}
+      addDeck={name => store.dispatch(addDeck(name))}
+      showAddDeck={() => store.dispatch(showAddDeck())}
+      hideAddDeck={() => store.dispatch(hideAddDeck())}
+    />
+   </App>, document.getElementById('root'));
+}
 
-ReactDOM.render(<App> <Sidebar decks={[ { name: 'Deck 1' } ]} addingDeck={true} />
- </App>, document.getElementById('root'));
+run();
+store.subscribe(run);
